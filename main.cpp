c8529b4a36d7e8d011e1df0e1f64dd24d183e757
Lukas Woodtli <woodtli.lukas@gmail.com>
2021-12-07 12:54:57 +0100

    Cleanup
    Change-Id: Ieeb873bfa64d941f4c4617c311cfea064c77e1e7

---
+++ main.cpp
@@ -0,0 +1,25 @@
+
+#include <Poco/ActiveRecord/Context.h>
+#include <Poco/File.h>
+#include "Sample/Role.h"
+#include <iostream>
+
+
+const constexpr auto connectionString = "/home/wolu/lab/poco-service-example/data.sqlite";
+
+int main() {
+    
+    if(not Poco::File(connectionString).exists())
+        std::cerr << "DB file doesn't exist: " << connectionString << std::endl;
+
+    
+    Poco::Data::Session session("SQLite", connectionString);
+    //Poco::Data::Session session("ODBC", "DSN=PostgreSQL");
+    Poco::ActiveRecord::Context::Ptr pContext = new Poco::ActiveRecord::Context(session);
+
+    Sample::Role::Ptr pDeveloper = new Sample::Role;
+    pDeveloper->name("Developer")
+        .description("Developer role");
+
+    pDeveloper->create(pContext);
+}
